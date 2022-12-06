cb7d6b013f40d21cc829612669cc5bbd347bb6e2
adauvigny <alex.dauvigny@gmail.com>
2022-12-06 18:34:41 +0100

    requirements.txt + début de fonction

--- API/app.py
+++ API/app.py
@@ -1,4 +1,4 @@
-#Useful functions for the API 
+#the real app
 
 import os
 
@@ -8,17 +8,10 @@ import pandas as pd
 from joblib import dump, load
 from sklearn.ensemble import GradientBoostingClassifier
 
-
 #DATA_FILEPATH = os.path.join('API', 'data', 'cs-training.csv')
 MODEL_FILEPATH_A = os.path.join('API', 'models', 'model.joblib')
 MODEL_FILEPATH_B = os.path.join('API', 'models', 'model.joblib')
 
-#features engineering 
-
-a ={"id":1, "ball_pos_x": -56.27}
 
 
-def data_treatment(params):
-    #params has to be a dictionary with the 55 features - well ordered
-    #{"id":1, "ball_pos_x": -56.27, ...}
-    print('nothing')
+app = Flask(__name__)
