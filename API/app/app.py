addf8f809ccd574971ff98196623fc9089125539
theok <Theophile.KARILA@student.isae-supaero.fr>
2022-12-08 13:35:18 +0100

    local API works

--- API/app/app.py
+++ API/app/app.py
@@ -7,7 +7,7 @@ import random
 import pandas as pd
 from joblib import dump, load
 from sklearn.ensemble import GradientBoostingClassifier
-
+import functions
 #DATA_FILEPATH = os.path.join('API', 'data', 'cs-training.csv')
 MODEL_FILEPATH_A = os.path.join('API', 'app','models', 'predictA.joblib')
 MODEL_FILEPATH_B = os.path.join('API', 'app','models', 'predictB.joblib')
@@ -19,5 +19,19 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     return "Welcome to the basic Flask app."
+
+
+@app.route('/predict', methods=['GET'])
+def get_score():
+    score = random.uniform(0, 1)
+    return jsonify({"score": score})
+
+@app.route('/predict', methods=['POST'])
+def get_scores():
+    payload = request.json
+    print(payload)
+    scores = functions.predict_scores(payload)
+    return scores
+
 
 
